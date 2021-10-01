bae90841747c20087853cd6f3c2d7935462c810b
hyunlee214 <hyun110214@naver.com>
2021-10-01 17:23:24 +0900

    test-app 추가생성 후 로그인 [새로운 FB_APP_ID]

--- src/public/fb.js
+++ src/public/fb.js
@@ -2,14 +2,23 @@
 
 window.fbAsyncInit = () => {
   FB.init({
-    appId      : '{your-app-id}',
-    cookie     : true,
-    xfbml      : true,
-    version    : '{api-version}'
+    appId      : APP_CONFIG.FB_APP_ID,
+    // cookie     : true,
+    // xfbml      : true,
+    version    : 'v12.0'
   });
-    
-  FB.AppEvents.logPageView();
-  
+
+  // index.pug 버튼이벤트
+  document.getElementById('fb-login').addEventListener('click', () => {
+  // facebook 로그인 시 할당받는 액세스토큰 + 액세스토큰 안의 유저 정보 필요
+  // FB.login() API 사용
+  FB.login(
+    (response) => {
+    console.log(response);
+    // handle the response
+  }, 
+    {scope: 'public_profile,email'});
+  })
 };
 
 ((d, s, id) => {
