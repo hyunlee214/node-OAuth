67a36e149dfc55286897079831d6774c145cd97e
hyunlee214 <hyun110214@naver.com>
2021-10-01 16:47:44 +0900

    fb SDK 설정 초기화

--- src/public/fb.js
+++ src/public/fb.js
@@ -1,5 +1,25 @@
-window.fbAsyncInit = () => {
-  /* TODO: implement */
-}
-
 // TODO: initialize Facebook SDK
+
+window.fbAsyncInit = () => {
+  FB.init({
+    appId      : '{your-app-id}',
+    cookie     : true,
+    xfbml      : true,
+    version    : '{api-version}'
+  });
+    
+  FB.AppEvents.logPageView();
+  
+};
+
+((d, s, id) => {
+   const fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) 
+   {return;}
+
+   const js = d.createElement(s)
+   js.id = id
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ })
+ (document, 'script', 'facebook-jssdk');
