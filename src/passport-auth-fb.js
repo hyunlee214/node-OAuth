3a17d869b111bdca011afc7da53291e946132495
hyunlee214 <hyun110214@naver.com>
2021-10-05 23:55:46 +0900

    passport파일 수정 + cbURL 추가

--- src/passport-auth-fb.js
+++ src/passport-auth-fb.js
@@ -1,13 +1,13 @@
 // @ts-check
 
-const passport = require('passport')
-const FacebookStrategy = require('passport-facebook').Strategy
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
 
 const {
   FB_APP_ID,
   FB_CLIENT_SECRET,
   getUserAccessTokenForFacebookAccessToken,
-} = require('./fb')
+} = require("./fb");
 
 /**
  * @param {import('express').Express} app
@@ -18,53 +18,60 @@ function setupFBAuth(app) {
       {
         clientID: FB_APP_ID,
         clientSecret: FB_CLIENT_SECRET,
-        callbackURL: '', // TODO: Use correct callback URL
+        // callbackURL - 절대경로여야 함 (fullURL)
+        // facebook 로그인 설정 > 클라이언트 OAuth 설정
+        callbackURL:
+          "https://8855-121-184-187-73.ngrok.io/passport/facebook/callback", // 유효한 OAuth redirect URI
       },
       (accessToken, refreshToken, profile, done) => {
         done(null, {
           facebook: {
             accessToken,
           },
-        })
+        });
       }
     )
-  )
+  );
 
-  app.use(passport.initialize())
+  app.use(passport.initialize());
 
+  // 직렬화
   passport.serializeUser((user, done) => {
-    done(null, user)
-  })
-
+    done(null, user);
+  });
+  // 비직렬화
   passport.deserializeUser((user, done) => {
-    done(null, user)
-  })
+    done(null, user);
+  });
 
   app.get(
-    '/passport/facebook',
-    passport.authenticate('facebook', { session: false })
-  )
+    "/passport/facebook",
+    passport.authenticate("facebook", { session: false })
+  );
 
+  // 로그인 종료 시
   app.get(
-    '/passport/facebook/callback',
-    passport.authenticate('facebook', {
+    "/passport/facebook/callback",
+    passport.authenticate("facebook", {
       session: false,
     }),
     async (req, res) => {
       // @ts-ignore
-      const fbAccessToken = req.user?.facebook?.accessToken
+      const fbAccessToken = req.user?.facebook?.accessToken;
+      // fbAccessToken이 있으면, 'get~Token' 함수 호출
       if (fbAccessToken) {
         const accessToken = await getUserAccessTokenForFacebookAccessToken(
           fbAccessToken
-        )
-        res.cookie('access_token', accessToken, {
+        );
+        res.cookie("access_token", accessToken, {
           httpOnly: true,
           secure: true,
-        })
-        res.redirect('/')
+        });
+        // 로그인 완료 시, 최종 redirect
+        res.redirect("/");
       }
     }
-  )
+  );
 }
 
-module.exports = setupFBAuth
+module.exports = setupFBAuth;
